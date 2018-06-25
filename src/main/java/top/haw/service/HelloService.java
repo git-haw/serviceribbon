0df1efa1671ab4ca8d5e1d61eec3e2e327433283
haw <haw358linux@163.com>
2018-06-25 13:37:31 +0800

    service consumer: ribbon+restTemplate

---
+++ src/main/java/top/haw/service/HelloService.java
@@ -0,0 +1,18 @@
+package top.haw.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by haw on 18-6-23.
+ */
+@Service
+public class HelloService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+}
---
+++ src/main/java/top/haw/web/HelloControler.java
@@ -0,0 +1,22 @@
+package top.haw.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import top.haw.service.HelloService;
+
+/**
+ * Created by haw on 18-6-23.
+ */
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+}
