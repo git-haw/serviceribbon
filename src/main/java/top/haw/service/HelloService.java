22f9433f992487325806b24135d09d483990588f
haw <haw358linux@163.com>
2018-06-26 21:25:53 +0800

    Hystrix Dashboard

--- src/main/java/top/haw/service/HelloService.java
+++ src/main/java/top/haw/service/HelloService.java
@@ -2,20 +2,30 @@ package top.haw.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Random;
 
 /**
  * Created by haw on 18-6-23.
  */
-@Service
+@Component
 public class HelloService {
+//    private Random random = new Random();
+
     @Autowired
     private RestTemplate restTemplate;
 
     @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+/*        int randomInt= random.nextInt(10) ;
+        if(randomInt<8){  //模拟调用失败情况
+            throw new RuntimeException("call dependency service fail.");
+        }else{
+            return "UserName:liaokailin;number:"+randomInt;
+        }*/
     }
 
     public String hiError(String name) {
